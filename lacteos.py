e099a7dd9456e010d966fb5fcdb56ab8093c2375
DavidNegreteL <david.negrete8@gmail.com>
2020-04-23 23:07:42 -0500

    Finalizado con archivos .CSV generados

--- lacteos.py
+++ lacteos.py
@@ -6,7 +6,6 @@ from time import sleep
 import datetime as dt
 import csv
 
-
 #Pins for motor Driver inputs (MX1508 | Chimalli's MotorB) BAND
 MotorBandA = 23
 MotorBandB = 24
@@ -14,6 +13,10 @@ MotorBandB = 24
 MotorValveA = 27
 MotorValveB = 22
 
+fig=plt.figure('Gráfica de llenado de envase')
+ax=fig.add_subplot(111)
+
+    
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
@@ -21,8 +24,8 @@ def setup():
     GPIO.setup(MotorBandB,GPIO.OUT)
     GPIO.setup(MotorValveA,GPIO.OUT)
     GPIO.setup(MotorValveB,GPIO.OUT)
-    fig=plt.figure('Gráfica de llenado de envase')
-    ax=fig.add_subplot(111)
+    
+    
     
 def moveBand(): #Sucess
     GPIO.output(MotorBandA,GPIO.LOW)
@@ -40,6 +43,9 @@ def closeValve(): #Sucess
     GPIO.output(MotorValveA,GPIO.HIGH)
     GPIO.output(MotorValveB,GPIO.LOW)
     
+def stopValve(): #Sucess
+    GPIO.output(MotorValveA,GPIO.LOW)
+    GPIO.output(MotorValveB,GPIO.LOW)
     
 def distance1(): #Sucess
     bandSensor = DistanceSensor(16,12)
@@ -58,37 +64,32 @@ def objectCounter(counter):
         register_object = csv.writer(registerObject, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         register_object.writerow(counterl)
 
-def sucessCounter():
-    scounter+=1
-    scounter = [scounter]
+def sucessCounter(scounter):
+    scounterl = [scounter]
     #print(temperatura)
     with open('sucessCounter.csv', mode='a' , newline='') as registerSucess:
         register_sucess = csv.writer(registerSucess, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        register_sucess.writerow(scounter)
+        register_sucess.writerow(scounterl)
         
-def errorCounter():
-    ecounter+=1
-    ecounter = [ecounter]
+def errorCounter(ecounter):
+    ecounterl = [ecounter]
     #print(temperatura)
     with open('errorCounter.csv', mode='a' , newline='') as registerError:
         register_error = csv.writer(registerError, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        register_error.writerow(ecounter)
+        register_error.writerow(ecounterl)
         
     
 def regHeight(distance2):
     height = [distance2]
     with open('height.csv', mode='a' , newline='') as registerHeight:
-        register_height = csv.writer(registerObject, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        register_height = csv.writer(registerHeight, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         register_height.writerow(height)
-def anima_regHeight():
-    ani=animation.FuncAnimation(fig,actualizar,interval=1)
-    plt.show()
     
 def actualizar(i):
     ax.clear()
-    distance2()
-    ax.plot(i,distance2)
-    ax.bar(1,distance2,width=0.5,align='center')
+    distance2r = distance2()
+    ax.plot(i,distance2r)
+    ax.bar(1,distance2r,width=0.5,align='center')
     plt.ylim(0,1.5)
     plt.ylabel('Altura del líquido (cm)')
     plt.xlim(0,2)
@@ -107,7 +108,28 @@ if __name__== '__main__':
             stopBand() #Se detiene la banda
             counter+=1 #Se incrementa el contador
             objectCounter(counter) #Contador de objetos que llegaron a la electroválvula | Registro en un .CSV
-            
+            distance2r = distance2()
+            if distance2r >= 20:
+                openValve()
+                sleep(1)
+                stopValve()
+                regHeight(distance2)
+                ani=animation.FuncAnimation(fig,actualizar,interval=1)
+                plt.show()
+                distance2r = distance2()
+                if distance2r <= 10: #Si el envase ya llegó al límite
+                    closeValve() #Se cierra la electroválcula
+                    sleep(1)
+                    stopValve()
+                    scounter+=1
+                    sucessCounter(scounter) #Contador de envases llenado de manera exitosa | Registro en un .CSV
+                    moveBand() #Avanza la banda
+                else:
+                    moveBand() #Avanza la banda
+            else:
+                ecounter+=1
+                errorCounter(ecounter)
+                moveBand()
         elif distance1r > 10:
             moveBand()
             flag = False
